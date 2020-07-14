da230ab36ca3f77f2844701bef97dd3e003e4300
Michael Famurewa <myfamurewa@gmail.com>
2020-07-14 18:34:20 -0400

    all tests passing

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -9,6 +9,11 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
+    def delete(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next: 
+            self.next.prev = self.prev
 
 
 """
@@ -70,92 +75,59 @@ class DoublyLinkedList:
             self.tail = new_list_node
 
     def remove_from_head(self):
-        # store the value of the head
-        old_head = self.head
-        #check to see if the DLL is empty
-        # delete the head
-        if self.head.next is not None:
-            self.head.next.prev = None
-        # set head to head.next
-            self.head = self.head.next
-        # this will delete it's last reference 
-        # which will trigger garbage cleanup
-        # decrement length
-            self.length -= 1
-        # return the value of the head
-            return old_head
+        if self.head:
+            value = self.head.value
         else:
-            self.head = None
-            self.tail = None
-            self.length = 0
-            return None
+            value = None
+        self.delete(self.head)
+        return value
 
     def remove_from_tail(self):
-        #store the value of the tail
-        old_tail = self.tail
-        # check to see if the DLL is empty
-        if self.tail.prev is not None:
-            self.tail.prev.next = None
-        # set tail to tail prev
-            self.tail = self.tail.prev
-        # decrement length
-            self.length -= 1
-        # return the value of the old tail
-            return add_to_tail
-        else:
-            self.head = None
-            self.tail = None
-            self.length = 0
-            return None
+        if self.tail:
+            value = self.tail.value
+        else: 
+            value = None
+        self.delete(self.tail)
+        return value
 
     """
     Removes the input node from its current spot in the List and inserts is as the new head node of the List.
     """
     def move_to_front(self, node):
-        if node is self.head:
-            return None
-        else:
-            # adjust the pointers of the elements touching the node
-            # set the next of the prev node to the node next
-            node.prev.next = node.next
-            node.next.prev = node.prev
-            # adjust the head (this logic should be similar to the logic for add to head)
-            node.next = self.head
-            # set head's prev to new node
-            self.head.prev = node
-            # set head to the new node
-            self.head = node
+        self.delete(node)
+        self.add_to_head(node.value)
 
 
     """
     Remove the input node from its current spot in the List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        if node is self.tail:
-            return None
-        else:
-            # adjust the pointers of the elements touching the node
-            # set the next of the prev node to the node next
-            node.prev.next = node.next
-            node.next.prev = node.prev
-            #adjust the tail (this logic should be similar to the logic for add to tail)
-            # Assign the newly created node to the current tail's next
-            self.tail.next = node
-            # Assign the current tail to the newly created node's prev
-            node.prev = self.tail
-            # Assign self.tail to the newly created node
-            self.tail = node
+        self.delete(node)
+        self.add_to_tail(node.value)
 
     """
     Deletes the input node from the List, preserving the order of the other elements of the List.
     """
     def delete(self, node):
-        # the logic here should be the same as move to end and move to staret without the head and tail adjustments
-        if node.prev.next and node.next:
-            node.prev.next = node.next
-        if node.next.prev and node.prev:
-            node.next.prev = node.prev
-        return "node was deleted"
+        # the logic here should be the same as move to end and move to start without the head and tail adjustments
+        # if empty
+        if not self.head and not self.tail:
+            return None
+        #if head and tail
+        if node == self.head and self.head == self.tail:
+            self.head = None
+            self.tail = None
+        elif node == self.head:
+            self.head = self.head.next
+            node.delete()
+        elif node == self.tail:
+            self.tail = self.tail.prev
+            node.delete()
+        else:
+            node.delete()
+
+        self.length -= 1
+
 
     """
     Finds and returns the maximum value of all the nodes in the list.
